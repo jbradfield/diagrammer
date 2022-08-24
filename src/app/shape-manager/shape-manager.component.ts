fc1af19c72673a8a4ab154078bf04e31015b8525
jbradfield <jbradfield@gmail.com>
2022-08-24 12:45:50 -0400

    closer.......

--- src/app/shape-manager/shape-manager.component.ts
+++ src/app/shape-manager/shape-manager.component.ts
@@ -1,6 +1,13 @@
-import {AfterViewInit, Component, ElementRef, OnChanges, OnInit, ViewChild} from '@angular/core';
-import {StorageService} from "../services/storage-service.service";
-import {environment} from "../../environments/environment";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { StorageService } from '../services/storage-service.service';
+import { environment } from '../../environments/environment';
 import { ShapeType } from '../enums/shape-type.enum';
 import { CanvasElement } from '../classes/canvas-element';
 import { CanvasComponent } from '../interfaces/canvas-component';
@@ -11,12 +18,11 @@ import { CanvasAnchorDirective } from '../directives/canvas-anchor.directive';
 @Component({
   selector: 'app-shape-manager',
   templateUrl: './shape-manager.component.html',
-  styleUrls: ['./shape-manager.component.scss']
+  styleUrls: ['./shape-manager.component.scss'],
 })
 export class ShapeManagerComponent implements OnInit, AfterViewInit {
-
   @ViewChild(CanvasAnchorDirective) canvas!: CanvasAnchorDirective;
-  
+
   shapeListKey: string;
   shapeList: CanvasElement[];
 
@@ -30,9 +36,7 @@ export class ShapeManagerComponent implements OnInit, AfterViewInit {
     this.shapeList = storageService.getData(this.shapeListKey) || [];
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
     this.draw();
@@ -43,34 +47,35 @@ export class ShapeManagerComponent implements OnInit, AfterViewInit {
     canvasViewContainer.clear();
 
     this.shapeList.forEach((element) => {
-      const comp = canvasViewContainer.createComponent<CanvasComponent>(element.type);
+      const comp = canvasViewContainer.createComponent<CanvasComponent>(
+        element.type
+      );
       comp.instance.shapeData = element.data;
-    })
+    });
   }
 
   addShape(type: string, x: string, y: string, h: string, w: string): void {
-    let element : CanvasElement;
-    if (type == "Rectangle") {
+    let element: CanvasElement;
+    if (type == 'Rectangle') {
       element = {
         type: RectangleComponent,
         data: {
-          x: parseFloat(x) - (parseFloat(w) / 2),
-          y: parseFloat(y) - (parseFloat(h) / 2),
+          x: parseFloat(x) - parseFloat(w) / 2,
+          y: parseFloat(y) - parseFloat(h) / 2,
           width: parseFloat(w),
-          height: parseFloat(h)
-        }
+          height: parseFloat(h),
+        },
       };
       this.shapeList.push(element);
-    }
-    else if (type == "Ellipse") {
+    } else if (type == 'Ellipse') {
       element = {
         type: EllipseComponent,
         data: {
           x: parseFloat(x),
           y: parseFloat(y),
           rx: parseFloat(w) / 2,
-          ry: parseFloat(h) / 2 
-        }
+          ry: parseFloat(h) / 2,
+        },
       };
       this.shapeList.push(element);
     }
@@ -84,6 +89,7 @@ export class ShapeManagerComponent implements OnInit, AfterViewInit {
 
   clear(): void {
     this.shapeList = [];
+    this.draw();
     this.saveList();
   }
 }
