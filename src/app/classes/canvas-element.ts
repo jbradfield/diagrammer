f567235189dc4039e8d37fae1c6056023eb16a25
jbradfield <jbradfield@gmail.com>
2022-08-25 12:08:35 -0400

    more cleanup

--- src/app/classes/canvas-element.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from "@angular/core";
-
-export class CanvasElement {
-  constructor(public type: Type<any>, public data: any) { }
-}
--- src/app/directives/dynamic-svg.directive.ts
+++ src/app/directives/dynamic-svg.directive.ts
@@ -1,17 +1,23 @@
-import { Directive, Input, OnDestroy, OnInit, Type, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewContainerRef,
+} from '@angular/core';
 import { ShapeComponent } from '../components/shape/shape.component';
 
 @Directive({
-  selector: '[dynamic-svg]'
+  selector: '[dynamic-svg]',
 })
 export class DynamicSvgDirective implements OnInit, OnDestroy {
-
   @Input() component: ShapeComponent;
 
-  constructor(private viewContainerRef: ViewContainerRef) { }
+  constructor(private viewContainerRef: ViewContainerRef) {}
 
   ngOnInit(): void {
-    this.viewContainerRef.createEmbeddedView(this.component.template)
+    console.log(this.component);
+    this.viewContainerRef.createEmbeddedView(this.component.template);
   }
 
   ngOnDestroy(): void {
